86df859766c4ef114eef84a7703217d99bd31057
Denis <156504836+Denis-Kotlyarov@users.noreply.github.com>
2024-02-13 13:02:54 +0000

    add base funct

--- scr/script.js
+++ scr/script.js
@@ -7,16 +7,29 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log('firstBTclick')
     })*/
 
+    function evaluateResult() {
+        let convertedValue = currentValue.replace("×", "*").replace("÷", "/").replace("%", "*0.01")
+        const result = eval(convertedValue)
+        currentValue = result.toString()
+        display.value = currentValue
+    }
+
     let currentValue = ''
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i]
         button.addEventListener('click', function() {
             const value = button.innerText
 
+            if (value == "AC") {
+                currentValue = ""
+                display.value = currentValue
+            } else if (value == "=") {
+                evaluateResult()
+            } else {
+                currentValue += value
+                display.value = currentValue
+            }
             
-
-            currentValue += value
-            display.value = currentValue
         })
     }
 })
