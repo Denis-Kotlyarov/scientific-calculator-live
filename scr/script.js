25722a8a676fc51d2a2f7a1996e4a099ff2a49ef
Denis <156504836+Denis-Kotlyarov@users.noreply.github.com>
2024-02-13 14:32:21 +0000

    a

--- scr/script.js
+++ scr/script.js
@@ -8,28 +8,64 @@ document.addEventListener("DOMContentLoaded", function() {
     })*/
 
     function evaluateResult() {
-        let convertedValue = currentValue.replace("×", "*").replace("÷", "/").replace("%", "*0.01")
+        let convertedValue = currentValue
+            .replace("×", "*")
+            .replace("÷", "/")
+            .replace("%", "*0.01")
+            .replace("sin", "Math.sin")
+            .replace("cos", "Math.cos")
+            .replace("ln", "Math.log")
+            .replace("π", "Math.PI")
+            .replace("log", "Math.log10")
+            .replace("e", "Math.E")
+            .replace("tan", "Math.tan")
+            .replace("√", "Math.sqrt")
         const result = eval(convertedValue)
         currentValue = result.toString()
         display.value = currentValue
     }
 
     let currentValue = ''
-    for (let i = 0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length - 2; i++) {
         const button = buttons[i]
         button.addEventListener('click', function() {
             const value = button.innerText
 
-            if (value == "AC") {
-                currentValue = ""
-                display.value = currentValue
-            } else if (value == "=") {
-                evaluateResult()
-            } else {
-                currentValue += value
+            try {
+                if (value == "AC") {
+                    currentValue = ""
+                    display.value = currentValue
+                } else if (value == "=") {
+                    evaluateResult()
+                } else {
+                    currentValue += value
+                    display.value = currentValue
+                }
+            } catch (error) {
+                console.error(error)
+                currentValue = "error"
                 display.value = currentValue
             }
             
         })
     }
+
+    const nums = this.getElementById('nums')
+    const numsPanel = this.getElementById('numsPanel')
+    const funcPanel = this.getElementById('funcPanel')
+    const func = this.getElementById('func')
+    nums.addEventListener('click', function() {
+        numsPanel.className = 'row gx-2 p-0 m-0'
+        funcPanel.className = 'row gx-2 p-0 m-0 d-none'
+
+        nums.className = 'btn btn-primary btn-sm mt-3'
+        func.className = 'btn btn-ligth border btn-sm mt-3'
+    })
+    func.addEventListener('click', function() {
+        numsPanel.className = 'row gx-2 p-0 m-0 d-none'
+        funcPanel.className = 'row gx-2 p-0 m-0'
+
+        nums.className = 'btn btn-ligth border btn-sm mt-3'
+        func.className = 'btn btn-primary btn-sm mt-3'
+    })
 })
